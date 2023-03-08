095cf343902e6d96147155da3bf62d79a18f6199
Marcus Little <marcuslittle@marcuss-mbp-2.lan>
2023-03-08 10:17:33 -0500

    add rewrite

--- gatsby-node.js
+++ gatsby-node.js
@@ -15,18 +15,11 @@ exports.createPages = async ({ actions }) => {
     context: {},
     defer: true,
   })
-
-  // sends all traffic from https://examplesite14960.gatsbyjs.io/it to https://italiansite.gatsbyjs.io/it/
-  // using a pathPrefix may prevent the need for this proxy
   createRedirect({
-    fromPath: `/it/`,
-    toPath: `https://italiansite.gatsbyjs.io/it/`,
+    fromPath: `/test`,
+    toPath: `https://www.gatsbyjs.com/docs/how-to/cloud/working-with-redirects-and-rewrites/`,
     statusCode: 200,
-  })
-  // sends all traffic from https://examplesite14960.gatsbyjs.io/it/* to https://italiansite.gatsbyjs.io/it/*
-  createRedirect({
-    fromPath: `/it/*`,
-    toPath: `https://italiansite.gatsbyjs.io/it/*`,
-    statusCode: 200,
-  })
+  });
 }
+
+
